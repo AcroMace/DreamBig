66582e3c5e343216b14df7f92f86e9149417f369
Andy Cho <acromace@gmail.com>
2017-06-10 18:27:45 -0400

    Create a DREAM BIG emoji text 5 meters away from you

--- DreamBig/Scene.swift
+++ DreamBig/Scene.swift
@@ -10,30 +10,63 @@ import SpriteKit
 import ARKit
 
 class Scene: SKScene {
-    
+
+    var identifierToEmoji = [UUID: String]()
+
+    let rows = 10
+    let columns = 21
+    let grid = "☁️☁️  ☁️☁️☁️ ☁️☁️☁️ ☁️☁️☁️ ☁️☁️ ☁️☁️" +
+               "☁️ ☁️ ☁️ ☁️ ☁️  ️ ☁️ ☁️ ☁️ ☁️ ☁️" +
+               "☁️ ☁️ ☁️☁️  ☁️☁️☁️ ☁️☁️☁️ ☁️ ☁️ ☁️" +
+               "☁️ ☁️ ☁️ ☁️ ☁️   ☁️ ☁️ ☁️ ☁️ ☁️" +
+               "☁️☁️  ☁️ ☁️ ☁️☁️☁️ ☁️ ☁️ ☁️ ☁️ ☁️" +
+               "     🙌🙌🙌 🙌🙌🙌 🙌🙌🙌     " +
+               "     🙌 🙌  🙌  🙌       " +
+               "     🙌🙌   🙌  🙌🙌🙌     " +
+               "     🙌 🙌  🙌  🙌 🙌     " +
+               "     🙌🙌🙌 🙌🙌🙌 🙌🙌🙌     "
+
     override func didMove(to view: SKView) {
         // Setup your scene here
     }
-    
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.removeAllChildren()
+
+        guard grid.count == rows * columns else {
+            print("The grid count was \(grid.count) but expected \(rows * columns) characters")
+            return
+        }
+        for (index, emoji) in grid.enumerated() {
+            addPoint(x: 0.5 * (Float(index % columns) - Float(columns / 2)),
+                     y: 0.5 * (Float(rows / 2) - Float(index / columns)),
+                     z: -5,
+                     emoji: emoji)
+        }
+    }
+
+    func addPoint(x: Float, y: Float, z: Float, emoji: Character) {
         guard let sceneView = self.view as? ARSKView else {
             return
         }
-        
+
         // Create anchor using the camera's current position
         if let currentFrame = sceneView.session.currentFrame {
-            
+
             // Create a transform with a translation of 0.2 meters in front of the camera
             var translation = matrix_identity_float4x4
-            translation.columns.3.z = -0.2
+            translation.columns.3.x = x
+            translation.columns.3.y = y
+            translation.columns.3.z = z
             let transform = simd_mul(currentFrame.camera.transform, translation)
-            
+
             // Add a new anchor to the session
             let anchor = ARAnchor(transform: transform)
+            identifierToEmoji[anchor.identifier] = String(emoji)
             sceneView.session.add(anchor: anchor)
         }
     }
--- DreamBig/ViewController.swift
+++ DreamBig/ViewController.swift
@@ -11,69 +11,75 @@ import SpriteKit
 import ARKit
 
 class ViewController: UIViewController, ARSKViewDelegate {
-    
+
+    var scene: Scene?
     @IBOutlet var sceneView: ARSKView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Set the view's delegate
         sceneView.delegate = self
-        
+
         // Show statistics such as fps and node count
         sceneView.showsFPS = true
         sceneView.showsNodeCount = true
-        
+
         // Load the SKScene from 'Scene.sks'
-        if let scene = SKScene(fileNamed: "Scene") {
+        scene = SKScene(fileNamed: "Scene") as? Scene
+        if scene != nil {
             sceneView.presentScene(scene)
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         // Create a session configuration
         let configuration = ARWorldTrackingSessionConfiguration()
-        
+
         // Run the view's session
         sceneView.session.run(configuration)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
         // Pause the view's session
         sceneView.session.pause()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-    
+
     // MARK: - ARSKViewDelegate
-    
+
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
         // Create and configure a node for the anchor added to the view's session.
-        let labelNode = SKLabelNode(text: "👾")
+        guard let emoji = scene?.identifierToEmoji[anchor.identifier] else {
+            return nil
+        }
+        let labelNode = SKLabelNode(text: emoji)
         labelNode.horizontalAlignmentMode = .center
         labelNode.verticalAlignmentMode = .center
-        return labelNode;
+        labelNode.fontSize = 200
+        return labelNode
     }
-    
+
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
+
     }
-    
+
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+
     }
-    
+
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+
     }
 }
