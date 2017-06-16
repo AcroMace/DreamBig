0e8326efc43c2db9de6dbc1eccef57ba508d5567
Andy Cho <acromace@gmail.com>
2017-06-16 11:26:33 -0400

    Add Swiftlint

--- DreamBig/Drawing/DrawingViewController.swift
+++ DreamBig/Drawing/DrawingViewController.swift
@@ -9,19 +9,22 @@
 import UIKit
 
 class DrawingViewController: UIViewController {
-    
+
     @IBOutlet weak var drawingImageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     @IBAction func didPressDoneButton(_ sender: Any) {
         // Trying to edit the "Main" storyboard currently crashes Xcode so the name can't be changed
         // Just use a transition and consolidate the two UIViewControllers to a single storyboard when the next beta comes out
-        let mainViewController = UIStoryboard(name: "Main", bundle: Bundle(for: ViewController.self))
-            .instantiateInitialViewController() as! ViewController
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle(for: ViewController.self))
+        guard let mainViewController = mainStoryboard.instantiateInitialViewController() as? ViewController else {
+            print("Could not create the Main view controller")
+            return
+        }
         navigationController?.pushViewController(mainViewController, animated: true)
     }
-    
+
 }
