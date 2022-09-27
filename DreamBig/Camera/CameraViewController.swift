9c849e235cccdefe1632f6aeff5afa25b242c912
Andy Cho <hello@andycho.ca>
2022-09-27 18:19:17 +0900

    Fix compiler errors for Xcode 14

--- DreamBig/Camera/CameraViewController.swift
+++ DreamBig/Camera/CameraViewController.swift
@@ -52,7 +52,7 @@ class CameraViewController: UIViewController {
         super.viewWillAppear(animated)
 
         // Create a session configuration
-        let configuration = ARWorldTrackingSessionConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
         sceneView.session.run(configuration)
